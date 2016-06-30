12459274522509f97d0614d9c6de6f270eadb896
Christoffer Lilja <c.lilja@piratplatsen.se>
2016-07-01 01:10:08 +0200

    Revision bump

--- bhweb.py
+++ bhweb.py
@@ -18,7 +18,7 @@ class base():
   def __init__(self):
     self.test = 0
     self.Config = ConfigParser.ConfigParser()
-    self.version = '0.3rev2'
+    self.version = '0.3rev3'
 
   def readconfig(self):
     self.Config.read(args.bithorded_config)
